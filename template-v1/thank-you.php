2604bbb6acd1c30788d9ee1e316c99af16f9eaea
Yari Andres <yari.andres@gmail.com>
2019-11-21 19:29:56 +0100

    fixing typos to thank page

--- template-v1/thank-you.php
+++ template-v1/thank-you.php
@@ -47,9 +47,9 @@
         <div class="container">    
             <div class="header text-center">
                 <hr id="black-hr">
-                <h1 class "main-head">Thanks for Grabbing This Deal!
+                <h1 class="main-head letter-spacing">Thanks for Grabbing This Deal!
                     <br>
-                    <span class="thing-text">Here is your coupon code &amp; instructions:</span>
+                    <span class="thing-text letter-spacing">Here is your coupon code &amp; instructions:</span>
                 </h1>
                 <hr id="black-hr">
             </div>
@@ -57,7 +57,7 @@
             <section>
                 <div class="paragraph">
         
-                    <p>This page provides your coupon code and instructions on how to claim this Amazon.com deal. </p>
+                    <p class="letter-spacing">This page provides your coupon code and instructions on how to claim this Amazon.com deal. </p>
                     <p>NOTE: Due to the limited nature of this Amazon deal, the coupon provided on this page must be used before the
                         deadline shown below:</p>
         
@@ -68,11 +68,11 @@
         
                 <hr>
         
-                <div class="paragraph">
+                <div class="paragraph letter-spacing">
                     <h2 class="text-center red-text">Instructions &amp; Your Coupon Code:</h2>
                     <p>
-                        <b> 1. Add ONE items to cart.</b> For the coupon to work properly, you must add 2 quantity of this product to
-                        your shopping cart. </p>
+                        <b> 1. Add ONE item to cart.</b> For the coupon to work properly, you must add a quantity of 1 to
+                        your shopping cart.</p>
                 </div>
         
                 <div class="product-img text-center">
@@ -80,24 +80,24 @@
                 </div>
         
                 <div class="paragraph">
-                    <p>
+                    <p class="letter-spacing">
                         <b>2. Use this coupon code at checkout.</b> You will find your special coupon code below. You will enter this
                         coupon code at checkout. When entered, this coupon will instantly apply the discount for buy 1 and get another
                         at 10% Off!</p>
                 </div>
         
                 <div id="green-background">
-                    <p class="yell-txt text-center">COPY Your COUPON CODE:
+                    <p class="yell-txt text-center letter-spacing">COPY Your COUPON CODE:
                         <b>
                             <br>
-                            <span class="red-text">THANKYUO</span>
+                            <span class="red-text letter-spacing">THANKYUO</span>
                         </b>
                     </p>
                 </div>
         
                 <hr>
         
-                <h3 class="text-center">Here's a screenshot from Amazon showing where you will enter the coupon code:</h3>
+                <h3 class="text-center letter-spacing">Here's a screenshot from Amazon showing where you will enter the coupon code:</h3>
         
                 <div class="product-img text-center">
                     <img src="img/coupon.jpg" height="160px" width="380">
@@ -109,7 +109,7 @@
                     <a href="https://www.amazon.co.uk/s/ref=sr_in_-2_p_6_16?fst=as%3Aoff&rh=n%3A65801031%2Ck%3ATurmeric+Curcumin+10%5Cc000mg%2Cp_6%3AA1YEPN8JHDSWMC&keywords=Turmeric+Curcumin+10%2C000mg&ie=UTF8">
         
                         <button class="btn-button">
-                            <span class="btn-text">Click Here To Vist amazon.com Page to Purchase This Deal</span>
+                            <span class="btn-text letter-spacing">Click Here To Vist amazon.com Page to Purchase This Deal</span>
                         </button>
                     </a>
                 </div>
@@ -120,7 +120,7 @@
                 <!-- footer section seven -->
         
                 <footer class="section-seven">
-                    <p class="text-center">Copyright © 2018
+                    <p class="text-center letter-spacing">Copyright © 2018
                         <span class="text-bold">AMz Accelerator Funnels </span> | Privacy | Design by
                         <a href="https://www.linkedin.com/in/yari-herrera-9677a9160" target="_blank">Yari Herrera</a>
                     </p>
